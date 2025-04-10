07830b2637ba86942e5bc1f36f2856413b5ce8fb
zhushuang <974198603@qq.com>
2025-04-10 15:38:46 +0800

    feat: Add support for NCCL test build and test results record

--- batch_test/test_nccl.sh
+++ batch_test/test_nccl.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
-NCCL_BUILD_DIR="/home/test/test01/zhushuang/workspace/nccl-tests/build"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-LOG_FILE="/home/test/test01/zhushuang/workspace/nccl-tests/batch_test/test_nccl.log"
+# set the NCCL test build directory and log file path
+NCCL_BUILD_DIR="$SCRIPT_DIR/../build"
+LOG_FILE="$SCRIPT_DIR/test_nccl.log"
 
-exec > >(tee "$LOG_FILE") 2>&1
+# set the root path of CUDA and NCCL (modified according to actual changes)
+CUDA_HOME="/usr/local/cuda"
+NCCL_HOME="/opt/nvidia/hpc_sdk/Linux_x86_64/23.5/comm_libs/12.1/nccl"
 
+# set the dynamic library search path (make sure libcuda and libnccl are found at runtime)
+export LD_LIBRARY_PATH="$CUDA_HOME/lib64:$NCCL_HOME/lib:$LD_LIBRARY_PATH"
+
+# # Print status messages
+echo "[INFO] Building NCCL tests..."
+echo "[INFO] CUDA_HOME=$CUDA_HOME"
+echo "[INFO] NCCL_HOME=$NCCL_HOME"
+echo "[INFO] NCCL_BUILD_DIR=$NCCL_BUILD_DIR"
+echo "[INFO] Log file: $LOG_FILE"
+
+# execute make build
+make -C "$SCRIPT_DIR/../src" build \
+  BUILDDIR="$NCCL_BUILD_DIR" \
+  CUDA_HOME="$CUDA_HOME" \
+  NCCL_HOME="$NCCL_HOME"
+
+# make sure NCCL test build file exeist 
 if [ ! -d "$NCCL_BUILD_DIR" ]; then
     echo "Error: NCCL_BUILD_DIR does not exist."
     exit 1
 fi
 
+# execute NCCL test
 find "$NCCL_BUILD_DIR" -name "*_perf" | sort | while read -r file; do
     echo "[Processing file: $file]"
-    ./build/all_gather_perf -b 8 -e 1M -f 2 -g 8
+    "$file" -b 8 -e 1M -f 2 -g 8
 done
 
-echo "All projects have been test." | tee -a "$LOG_FILE"
+# record NCCL test result in log file
+exec > >(tee "$LOG_FILE") 2>&1
+echo "[INFO] All projects have been test" | tee -a "$LOG_FILE"
